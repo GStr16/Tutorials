cc37bbf483c50d067204ce598adda7fe837714c6
GStr16 <45490904+GStr16@users.noreply.github.com>
2019-12-29 20:47:02 +0200

    Add files via upload

--- ScannerIO.java
+++ ScannerIO.java
@@ -1,22 +1,27 @@
-//Importing java.util.Scanner, with this library you can find the Scanner class, With Scanner helps you can input data from your keyboard
+package Tutorial;
+
+//import java.util.*;
 import java.util.Scanner;
+
 public class ScannerIO {
-    // main function
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); //This is the Object when is created a references with sc names from System.in using Scanner
 
-        System.out.print("Enter your name: "); // Output messages
-        String name = sc.nextLine(); // Declare the String variable
-        System.out.println("Your name is "+name); // Output the name variable
+        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter your age: "); // Output messages
-        int age = sc.nextInt(); // Declare the Integer variable
-        System.out.println("Your age is "+age);// Output the age variable
+        System.out.println("Enter your name:");
+        String name = sc.nextLine();
+        System.out.println("Your name is "+ name);
+        System.out.println("Enter your favorite color");
+        String color = sc.next();
+        System.out.println("Your color is "+color);
 
-        System.out.print("Enter you height: ");// Output messages
-        float height = sc.nextFloat(); // Declare the Float variable
-        System.out.println("Your height is "+height); // Output the height variable
+        System.out.println("Enter you age");
+        int age = sc.nextInt();
+        System.out.println("Your age is "+age);
 
-
+        System.out.println("Enter your height:");
+        float height = sc.nextFloat();
+        System.out.println("Your height is "+height);
     }
 }
