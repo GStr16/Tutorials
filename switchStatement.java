fa77520e45998cc9ec76fd59cbfb55f61753c5e7
GStr16 <45490904+GStr16@users.noreply.github.com>
2020-01-03 14:36:46 +0200

    Update

--- switchStatement.java
+++ switchStatement.java
@@ -1,49 +1,22 @@
-package Tutorial;
-
+//import library from input my data from my keyboard
 import java.util.Scanner;
+
 
 public class switchStatement {
 
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
+//    Create an Object with the Scanner Class
+    static Scanner sc = new Scanner(System.in);
+//    main function
+    public static void main(String[] args) {
+        System.out.println("Enter your command: ");
+        int command = sc.nextInt();
 
-
-        while(true) {
-
-            System.out.println("Enter your command:");
-            int x = sc.nextInt();
-
-            switch (x) {
-                case 1: {
-                    System.out.println("Hello World");
-                    break;
-                }
-                case 2 :
-                    System.exit(0);
-                    break;
-                case 3: {
-                    System.out.println("Enter your numbers!");
-                    int b = sc.nextInt();
-                    int a = sc.nextInt();
-                    int sum = a+b;
-                    System.out.println(sum);
-                    break;
-                }
-                case 4 :{
-                    System.out.println("Enter your numbers!");
-                    int b = sc.nextInt();
-                    int a = sc.nextInt();
-                    int mul = a*b;
-                    System.out.println(mul);
-                    break;
-                }
-                case 5 :{
-                    System.out.println("Enter your name!");
-                    String name = sc.next();
-                    System.out.println("Your name is "+ name);
-                    break;
-                }
-            }
+        switch (command) {
+            case 1:
+                System.out.println("Hello World");
+                break;
+            case 2:
+                System.exit(0); //close your program
         }
     }
 }
